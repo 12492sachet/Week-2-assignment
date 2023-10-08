62ce9c8fa95a40cb8d0f3c4f65b5cef6e0005118
eve-atieno <atienoevelyne53@gmail.com>
2023-10-08 12:15:33 +0300

    made changes

---
+++ Assignement.js
@@ -0,0 +1,10 @@
+// assigning a string to a number variable
+var numberStr = "12";
+ console.log( numberStr);
+
+// converting the given value to a number.
+var convertedNumber = Number(numberStr);
+console.log( convertedNumber)
+
+
+
