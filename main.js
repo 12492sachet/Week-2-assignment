14110fa8f3c1b28f8915ca0d826c09067864741c
lewis machabe <lewismachabe@gmail.com>
2023-10-09 09:16:08 +0300

    converstion to a boolean

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ const NumAsString = string(num);
 //Displaying the converted string in the console.
 
 console.log("converted string is:" + NumAsString);
+
+
+
+//Declare a variable `truthyValue` and assign it a truthy value.
+const truthyValue = "Sachet";
+
+// Declare a variable `falsyValue` and assign it a falsy value.
+const falsyValue = 0;
+
+//Useing explicit type conversion to convert truthyValue to a boolean using the Boolean function.
+const truthyBoolean = Boolean(truthyValue);
+
+//  Useing explicit type conversion to convert falsyValue to a boolean using the Boolean function.
+const falsyBoolean = Boolean(falsyValue);
+
+//Displaying the converted boolean values in the console.
+console.log("truthy value as boolean:", truthyBoolean); // it will display "truthy value as boolean: true"
+console.log("falsy value as boolean:", falsyBoolean);   // it will display "falsy value as boolean: false"
+
+
+
+
+
