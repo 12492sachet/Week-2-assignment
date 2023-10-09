0e477dce8e288a29b09a307e68c1f0dd04cca266
lewis machabe <lewismachabe@gmail.com>
2023-10-09 07:16:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Declaring a variable number and assigning it  a value.
+const numberStr = '36';
+
+//using explicit type conversion to convert "numberStr" to a number using the number function.
+
+const convertedNumber = Number(numberStr);
+
+//Displaying the converted number in th console.
+
+console.log(convertedNumber);
