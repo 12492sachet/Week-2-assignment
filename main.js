088b307a7e90adc51af32d8b6557d697e661ac99
lewis machabe <lewismachabe@gmail.com>
2023-10-09 08:46:17 +0300

    converting a number to a string

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ const convertedNumber = Number(numberStr);
 //Displaying the converted number in th console.
 
 console.log(convertedNumber);
+
+
+//Declare a variable 'num' and assign it a numerical value.
+const num = 2023;
+
+//explicit type conversion to convert 'num' to a string using the 'String()' function.
+
+const NumAsString = string(num);
+
+//Displaying the converted string in the console.
+
+console.log("converted string is:" + NumAsString);
